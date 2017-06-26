f8b85e0b9ba6897565e009767a8be05bb530004e
Uldza <uldis.megnis@gmail.com>
2017-06-26 08:28:05 +0300

    Modify defconfig and post-createfs

--- post-createfs.sh
+++ post-createfs.sh
@@ -9,5 +9,8 @@ $HOST_DIR/usr/bin/mkimage -A arm -O linux -T script -C none -a 0 -e 0 \
     -n "nerves boot script" -d $NERVES_DEFCONFIG_DIR/uboot-script.cmd \
     $BINARIES_DIR/boot.scr
 
+cp $NERVES_DEFCONFIG_DIR/board/uEnv.txt $BINARIES_DIR
+cp $NERVES_DEFCONFIG_DIR/board/system.bit $BINARIES_DIR
+
 # Run the common post-image processing for nerves
 $BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
