33e1a53d09016c38f5711025bcc4c032eb18601b
Uldza <uldis.megnis@gmail.com>
2017-06-29 10:19:28 +0300

    Update uboot-script.cmd file

--- uboot-script.cmd
+++ uboot-script.cmd
@@ -1,9 +1,5 @@
 # Custom U-Boot startup script for Nerves
 
-# This script is invoked from the default AM335x build script
-# in "<uboot>/include/configs/am335x_evm.h". In the default build
-# script, look for `mmcboot` and its invocation of `loadbootscript`.
-
 # It has been simplified to just support the Nerves use cases.
 # Most importantly, it will not fallback to the alternate boot location
 # like the default script. If you have it installed on the eMMC
@@ -18,12 +14,7 @@ echo "Running Nerves U-Boot script"
 # For example, add "-v" to the end to put erlinit into verbose
 # mode.
 
-# When debugging, create a uEnv.txt file in the BOOT partition
-# to override these arguments. For example, if you want the
-# kernel debug messages, put this in a uEnv.txt:
-#   optargs="bone_capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN"
-
-env set optargs "quiet bone_capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN"
+# At the moment booting is done by uEnv.txt file
 
 # Allow the user to override the kernel/erlinit arguments
 # via a "uEnv.txt" file in the FAT partition.
@@ -37,23 +28,3 @@ if load mmc ${mmcdev}:1 ${loadaddr} uEnv.txt; then
         run uenvcmd
     fi
 fi
-
-# Determine the boot arguments
-#
-# Note the root filesystem specification. In Linux, /dev/mmcblk0 is always
-# the boot device. In uboot, mmc 0 is the SDCard and mmc 1 is the eMMC.
-# Therefore, we hardcode root=/dev/mmcblk0p2 since we always want to mount
-# the root partition off the same device that ran u-boot and supplied
-# uImage.
-setenv bootargs console=${console} ${optargs} root=/dev/mmcblk0p2 rootfstype=squashfs ro rootwait
-
-# Load the kernel
-load mmc ${mmcdev}:1 ${loadaddr} uImage
-
-# Load the DT. On the BBB, fdtfile=am335x-boneblack.dtb
-load mmc ${mmcdev}:1 ${fdtaddr} ${fdtfile}
-
-# Boot!!
-bootz ${loadaddr} - ${fdtaddr}
-
-echo "Nerves boot failed!"
